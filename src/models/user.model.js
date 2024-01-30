45b63b28379c2bc35a6f3661a7d62a859dc017c6
vivek kumar sah <viveksah4ui@gmail.com>
2024-01-30 12:59:45 +0530

    add multer or cloudenary

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,39 +53,38 @@ userSchema.pre("save", function (next) {
   next();
 });
 
-//user add password is string but we are store bcrypt how to login another time to compare brypt and user password 
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password , this.password)
-}
+//user add password is string but we are store bcrypt how to login another time to compare brypt and user password
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 //token generate
 
-userSchema.methods.generateAccessToken=function(){
-    return jwt.sign(
-        {
-            _id : this._id,
-            email: this.email,
-            fullName: this.fullName,
-            userName: this.userName
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRE
-        }
-    )
-}
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      fullName: this.fullName,
+      userName: this.userName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRE,
+    }
+  );
+};
 // refresh token generate
-userSchema.methods.generateRefreshToken=function(){
-    return jwt.sign(
-        {
-            _id : this._id,
-           
-        },
-        process.env.REFRESH_TOKEN_SYSTEM,
-        {
-            expiresIn: process.env.REFRESH_TOKEN_EXPIRE
-        }
-    )
-}
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SYSTEM,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRE,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
